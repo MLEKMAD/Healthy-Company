e18525778514c7b1e1628b7863bb76f0cac1ca98
lahlali hatim <hatimlahlali.13@outlook.fr>
2020-06-21 14:26:14 +0100

    backend V2

--- User_service/src/test/java/com/example/demo/UserServiceApplicationTests.java
+++ User_service/src/test/java/com/example/demo/UserServiceApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
-import java.util.Collections;
+//import java.util.Collections;
 
+import org.springframework.boot.test.context.SpringBootTest;
+/*
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,10 +14,10 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.example.demo.repositories.EmployeRepository;
-
+*/
 @SpringBootTest
 class UserServiceApplicationTests {
-
+/*
 	@Autowired
 	MockMvc mockMvc;
 	@MockBean
@@ -34,5 +35,5 @@ class UserServiceApplicationTests {
 			
 			Mockito.verify(employeRepository).findAll();
 	}
-
+*/
 }
