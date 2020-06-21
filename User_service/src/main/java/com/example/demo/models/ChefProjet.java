03557756821f0056a5d3ff106f330d3efff92398
lahlali hatim <hatimlahlali.13@outlook.fr>
2020-06-21 19:35:13 +0100

    refactor

--- User_service/src/main/java/com/example/demo/models/ChefProjet.java
+++ User_service/src/main/java/com/example/demo/models/ChefProjet.java
@@ -41,46 +41,54 @@ public class ChefProjet {
 
 
 
-	public long getId_chef_projet() {
-		return id_chef_projet;
-	}
-
-	public void setId_chef_projet(long id_chef_projet) {
-		this.id_chef_projet = id_chef_projet;
-	}
-
 	public String getNom_chef_projet() {
 		return nom_chef_projet;
 	}
+
+
 
 	public void setNom_chef_projet(String nom_chef_projet) {
 		this.nom_chef_projet = nom_chef_projet;
 	}
 
+
+
 	public String getPrenom_chef_projet() {
 		return prenom_chef_projet;
 	}
+
+
 
 	public void setPrenom_chef_projet(String prenom_chef_projet) {
 		this.prenom_chef_projet = prenom_chef_projet;
 	}
 
+
+
 	public String getEmail_chef_projet() {
 		return email_chef_projet;
 	}
+
+
 
 	public void setEmail_chef_projet(String email_chef_projet) {
 		this.email_chef_projet = email_chef_projet;
 	}
 
+
+
 	public String getPassword_chef_projet() {
 		return password_chef_projet;
 	}
 
+
+
 	public void setPassword_chef_projet(String password_chef_projet) {
 		this.password_chef_projet = password_chef_projet;
 	}
-	
+
+
+
 
 
 }
