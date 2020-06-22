d747859966b6dbb5791163dba876ee8c4ea55590
MahfoudAyoub <60109121+MahfoudAyoub@users.noreply.github.com>
2020-06-22 18:27:00 +0200

    commit

--- frontend/src/AppProfile.js
+++ frontend/src/AppProfile.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
+import React, { Component } from "react";
+import classNames from "classnames";
 
 export class AppProfile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      expanded: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            expanded: false
-        };
-        this.onClick = this.onClick.bind(this);
-    }
+  onClick(event) {
+    this.setState({ expanded: !this.state.expanded });
+    event.preventDefault();
+  }
 
-    onClick(event) {
-        this.setState({expanded: !this.state.expanded});
-        event.preventDefault();
-    }
-
-    render() {
-        return  (
-            <div className="layout-profile">
-                <div>
-                    <img src="assets/layout/images/avatar_4.png" alt="" />
-                </div>
-                <button className="p-link layout-profile-link" onClick={this.onClick}>
-                    <span className="username">LAHLALI Hatim</span>
-                    <i className="pi pi-fw pi-cog"/>
-                </button>
-                <ul className={classNames({'layout-profile-expanded': this.state.expanded})}>
-                    <li><button className="p-link"><i className="pi pi-fw pi-power-off"/><span>Logout</span></button></li>
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="layout-profile">
+        <div>
+          <img src="assets/layout/images/avatar_4.png" alt="" />
+        </div>
+        <button className="p-link layout-profile-link" onClick={this.onClick}>
+          <span className="username">LAHLALI Hatim</span>
+          <i className="pi pi-fw pi-cog" />
+        </button>
+        <ul
+          className={classNames({
+            "layout-profile-expanded": this.state.expanded,
+          })}
+        >
+          <li>
+            <button className="p-link">
+              <i className="pi pi-fw pi-power-off" />
+              <span>Logout</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,46 +1,47 @@
-import 'react-app-polyfill/ie11';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import "react-app-polyfill/ie11";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 //import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom'
-import ScrollToTop from './ScrollToTop';
-import Signin  from './components/Singin';
-import Signup  from './components/Signup';
-import DashboardChefProjetApp  from './components/chefProjet/DashboardChefProjetApp';
-import DashboardEmpApp  from './components/Emp/DashboardEmpApp';
+import { HashRouter } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
+import Signin from "./components/Singin";
+import Signup from "./components/Signup";
+import DashboardChefProjetApp from "./components/chefProjet/DashboardChefProjetApp";
+import DashboardEmpApp from "./components/Emp/DashboardEmpApp";
 
-import Questionnaire  from './components/Questionnaire';
-import Container  from './components/Container';
-
+import Questionnaire from "./components/Questionnaire";
+import Container from "./components/Container";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-
 ReactDOM.render(
-        <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Signin} />
-                <Route exact path="/signup" component={Signup} />          
-                <Route exact path="/Dashboard" component={App} />
-                <Route exact path="/DashboardChefProjet" component={DashboardChefProjetApp} />
-                <Route exact path="/DashboardEmp" component={DashboardEmpApp} />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Signin} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/Dashboard" component={App} />
+      <Route
+        exact
+        path="/DashboardChefProjet"
+        component={DashboardChefProjetApp}
+      />
+      <Route exact path="/DashboardEmp" component={DashboardEmpApp} />
 
-                <Route exact path="/Questionnaire" component={Questionnaire} />
-                <Route exact path="/Container" component={Container} />
-
-
-                
-            </Switch>
-        </BrowserRouter>,
-    document.getElementById('root')
+      <Route exact path="/Questionnaire" component={Questionnaire} />
+      <Route exact path="/Container" component={Container} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
-{/* <HashRouter>
+{
+  /* <HashRouter>
 <ScrollToTop>
 <App></App>
         </ScrollToTop>
-    </HashRouter> */}
+    </HashRouter> */
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
