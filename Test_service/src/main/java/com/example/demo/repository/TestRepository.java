610c0753bc1c600ebca6ab7d052cb4729f8c293a
lahlali hatim <hatimlahlali.13@outlook.fr>
2020-06-22 14:43:40 +0100

    response question APi added

--- Test_service/src/main/java/com/example/demo/repository/TestRepository.java
+++ Test_service/src/main/java/com/example/demo/repository/TestRepository.java
@@ -10,10 +10,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestRepository extends JpaRepository<Test,Integer> {
-    @Query(nativeQuery = true ,value =  "SELECT * FROM files c where c.id_chef_projet =:id_chef_projet")
-    Optional<Test> findByIdChefProjet(@Param("id_chef_projet") String id_chef_projet);
+public interface TestRepository extends JpaRepository<Test,String> {
+    @Query(nativeQuery = true ,value =  "SELECT * FROM files c where c.id_file =:id_file")
+    Optional<Test> findByIdFile(@Param("id_file") String id_file);
 
+    @Query(nativeQuery = true ,value =  "SELECT max(*) FROM files c where c.id_chef_projet =:id_chef_projet")
+    Optional<Test> findMax(@Param("id_chef_projet") long id_chef_projet);
+   
     @Query(nativeQuery = true ,value =  "SELECT * FROM files f")
     Optional<Test> findFile();
 }
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import {CarService} from '../service/CarService';
 import {Panel} from 'primereact/panel';
 import {Checkbox} from 'primereact/checkbox';
-import {DataTable} from 'primereact/datatable';
-import {Column} from 'primereact/column';
-import dayGridPlugin from '@fullcalendar/daygrid';
-import interactionPlugin from '@fullcalendar/interaction';
-import timeGridPlugin from '@fullcalendar/timegrid';
 import {DataView, DataViewLayoutOptions} from 'primereact/dataview';
 import {Button} from 'primereact/button';
-import {Dropdown} from 'primereact/dropdown';
 import { Link } from "react-router-dom";
 
 export class Dashboard extends Component {
@@ -20,11 +13,11 @@ export class Dashboard extends Component {
             tasks: [],
             city: null,
             layout: 'list',
-            dv: [
-
-                { nom_prenom: "lala", email:"dsf@hotmail.Fr", etat:"sain"}
-            ]              
-                ,
+            data: [
+                { "nom_prenom": "lekmad mohamed", "email":"mohamed", "etat":"malade"},
+                { "nom_prenom": "sdfgdfsg", "email":"dsf", "etat":"malade"},
+                { "nom_prenom": "lala", "email":"dsf@hotmail.Fr", "etat":"sain"}
+            ],
             selectedCar: null,
         };
 
@@ -40,9 +33,50 @@ export class Dashboard extends Component {
 
         this.setState({tasks: selectedTasks});
     }
-    // componentDidMount() {
-    //     this.carservice.getCarsSmall().then(data => this.setState({cars: data}));
-    // }
+
+    dataViewItemTemplate(data,layout) {
+        let src = "assets/layout/images/avatar_4.png";
+        if (layout === 'list') {
+            return (
+                <div className="p-grid" style={{padding: '2em', borderBottom: '1px solid #d9d9d9'}}>
+
+                    <div className="p-col-12 p-md-3">
+                        <img src={src} />
+                    </div>
+                    <div className="p-col-12 p-md-8 car-details">
+                        <div className="p-grid">
+                            <div className="p-col-2 p-sm-6">Nom et prenom:</div>
+                            <div className="p-col-10 p-sm-6">{data.nom_prenom}</div>
+
+                            <div className="p-col-2 p-sm-6">email:</div>
+                            <div className="p-col-10 p-sm-6">{data.email}</div>
+
+                            <div className="p-col-2 p-sm-6">etat :</div>
+                            <div className="p-col-10 p-sm-6">{data.etat}</div>
+                        </div>
+                    </div>
+                    <div className="p-col-12 p-md-1 search-icon" style={{marginTop:'40px'}}>
+                        <Link to='/DashbordEmp' >
+                        <Button onClick={this.showSuccess} label="Go to his Dashboard" className="p-button-success" style={{width:'10em', marginRight:'.25em'}} />
+                    </Link>
+                    </div>
+                </div>
+            );
+        }
+        if (layout === 'grid') {
+            return (
+                <div style={{ padding: '.5em' }} className="p-col-12 p-md-3">
+                    <Panel header={data.nom_prenom} style={{ textAlign: 'center' }}>
+                        <img src={src} />
+                        <div className="car-detail">{data.email} - {data.etat}</div>
+                        <Link to='/DashbordEmp' >
+                        <Button onClick={this.showSuccess} label="Download test" className="p-button-success" style={{width:'10em', marginRight:'.25em'}} />
+                         </Link>
+                    </Panel>
+                </div>
+            );
+        }
+    }
 
 
     render() {      
@@ -53,6 +87,7 @@ export class Dashboard extends Component {
                 </div>
             </div>
         ); 
+        const data =this.state.data;
         return (
             <div className="p-grid p-fluid dashboard">
                 <div className="p-col-12 ">
@@ -69,7 +104,13 @@ export class Dashboard extends Component {
                         </ul>
                     </Panel>
                 </div>
-
+                    {/* <div className="p-col-12">
+                    <div className="card card-w-title">
+                        <h1>Liste des employés</h1>
+                        <DataView  value={data}  itemTemplate={this.dataViewItemTemplate} layout={this.state.layout}
+                                   paginator={true} rows={10} header={header} ></DataView>
+                    </div>
+                </div> */}
 
             </div>
         );
